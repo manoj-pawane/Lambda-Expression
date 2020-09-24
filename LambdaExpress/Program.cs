30331ec0c869e7242bc5f28f6ffd5858c79b9922
Manoj <manoj.pawane@bridgelabz.com>
2020-09-24 17:40:55 +0530

    Lambda expression code added

---
+++ LambdaExpress/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace LambdaExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Expression Lambda Example 1
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(5));
+
+            /// Expression Lambda Example 2
+            System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+            Console.WriteLine(e);
+
+            ///Lambda with LINQ Example
+            int[] numbers = { 2, 3, 4, 5 };
+            var squaredNumbers = numbers.Select(x => x * x);
+            Console.WriteLine(string.Join(" ", squaredNumbers));
+
+
+            ///Statement Lambda
+            Action<string> greet = name =>
+            {
+                string greeting = $"Hello {name}!";
+                Console.WriteLine(greeting);
+            };
+            greet("World");
+        }
+    }
+}
